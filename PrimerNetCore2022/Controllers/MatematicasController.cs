1f2ac9d6faf858349de29d917ba2dadac0746735
Serra <pacoserranox@gmail.com>
2022-10-03 20:58:29 +0200

    commit

--- PrimerNetCore2022/Controllers/MatematicasController.cs
+++ PrimerNetCore2022/Controllers/MatematicasController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PrimerNetCore2022.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,6 +21,44 @@ namespace PrimerNetCore2022.Controllers
             int suma = numero1 + numero2;
             ViewData["MENSAJE"] = "La suma de " + numero1 + " + " + numero2 + " es " + suma;
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult TablaMultiplicarEnteros()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult TablaMultiplicarEnteros(int numero)
+        {
+            List<int> coleccion = new List<int>();
+            for (int i = 1; i <= 10; i++)
+            {
+                int operacion = numero * i;
+                coleccion.Add(operacion);
+            }
+            return View(coleccion);
+        }
+
+        [HttpGet]
+        public IActionResult TablaMultiplicarObjetos()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult TablaMultiplicarObjetos(int numero)
+        {
+            List<ElementoTabla> coleccion = new List<ElementoTabla>();
+            for (int i = 1; i <= 10; i++)
+            {
+                ElementoTabla elemento = new ElementoTabla();
+                elemento.Operacion = numero + " * " + i;
+                elemento.Resultado = numero * i;
+                coleccion.Add(elemento);
+            }
+            return View(coleccion);
         }
     }
 }
---
+++ PrimerNetCore2022/Models/ElementoTabla.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrimerNetCore2022.Models
+{
+    public class ElementoTabla
+    {
+        public string Operacion { get; set; }
+        public int Resultado { get; set; }
+    }
+}
