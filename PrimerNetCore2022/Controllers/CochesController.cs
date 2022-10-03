ae310da5ad786a6e2c2e8d62d301d2b5160fa73b
Serra <pacoserranox@gmail.com>
2022-10-03 21:54:10 +0200

    commit

---
+++ PrimerNetCore2022/Controllers/CochesController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PrimerNetCore2022.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrimerNetCore2022.Controllers
+{
+    public class CochesController : Controller
+    {
+        //ESTA CLASE UTILIZAR OBJETOS DE TIPO COCHE
+        private Coche car;
+
+        //POR AHORA, NO VOY A UTILIZAR DI
+        public CochesController()
+        {
+            this.car = new Coche();
+        }
+
+        public IActionResult Index()
+        {
+            return View(this.car);
+        }
+
+        [HttpPost]
+        public IActionResult Index(string accion)
+        {
+            if (accion.ToLower() == "acelerar")
+            {
+                this.car.Acelerar();
+            }
+            else
+            {
+                this.car.Frenar();
+            }
+            return View(this.car);
+        }
+    }
+}
---
+++ PrimerNetCore2022/Models/Coche.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrimerNetCore2022.Models
+{
+    public class Coche
+    {
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public string Imagen { get; set; }
+        public int Velocidad { get; set; }
+        public int VelocidadMaxima { get; set; }
+        public Coche()
+        {
+            this.Marca = "BatMovil";
+            this.Modelo = "Antiguo";
+            this.Imagen = "batmovil.jpg";
+            this.Velocidad = 0;
+            this.VelocidadMaxima = 120;
+        }
+
+        public int Acelerar()
+        {
+            this.Velocidad += 20;
+            if (this.Velocidad >= this.VelocidadMaxima)
+            {
+                this.Velocidad = this.VelocidadMaxima;
+            }
+            return this.Velocidad;
+        }
+
+        public int Frenar()
+        {
+            this.Velocidad -= 20;
+            if (this.Velocidad < 0)
+            {
+                this.Velocidad = 0;
+            }
+            return this.Velocidad;
+        }
+    }
+}
