1c251cd6d4b299885b2e434e94820a6b63590616
Serra <pacoserranox@gmail.com>
2022-10-03 20:04:44 +0200

    commit

---
+++ PrimerNetCore2022/Controllers/MatematicasController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrimerNetCore2022.Controllers
+{
+    public class MatematicasController : Controller
+    {
+        [HttpGet]
+        public IActionResult SumarNumeros()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SumarNumeros(int numero1, int numero2)
+        {
+            int suma = numero1 + numero2;
+            ViewData["MENSAJE"] = "La suma de " + numero1 + " + " + numero2 + " es " + suma;
+            return View();
+        }
+    }
+}
